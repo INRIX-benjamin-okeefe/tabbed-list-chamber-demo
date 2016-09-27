c4f768fd42eb10cdcd157d599c9f2b63aa830860
Benjamin O'Keefe <benjamin.okeefe@inrix.com>
2016-09-27 14:22:49 -0700

    Improved data object readability

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ This app demonstrates the TabbedListChamber in its current state. It contains se
 2. Switch to the **tabbed-list-chamber** branch in ividev.
 3. Navigate to the downloaded app's directory in your Terminal or Command Prompt, and enter **node index** to start the local Nodejs server.
 4. Select **tabbed-list-chamber** from the launch app/workspace menu in the simulator.
+
+## Caveats
+1. The Tabbed List Chamber that backs this app is currently in an alpha state, so unexpected behavior may occur. 
+2. The list hijacks the commander focus, so it's difficult to use the commander to navigate the tabs.
--- myChamber.js
+++ myChamber.js
@@ -8,53 +8,67 @@ define(function (require) {
     		icon: 'arrows',
 		    enabled: true,
 		    selected: false,
-		    _data_tree_branch: [{
-		      text: 'New Tab Data 1'
-		    },{
-		      text: 'New Tab Data 2',
-		    },{
-		      text: 'New Tab Data 3',
-		      text1: 'more new tab data text'
-		    }]
+		    _data_tree_branch: [
+		    	{
+		      		text: 'New Tab Data 1'
+		    	},
+		    	{
+		      		text: 'New Tab Data 2',
+		    	},
+		    	{
+		      		text: 'New Tab Data 3',
+		      		text1: 'more new tab data text'
+		    	}
+		    ]
     	};
 
     let dataTree = [
 	    {
-	      text: 'Tab 1',
-	      icon: `${window.MODULE_PATH}/icons/analysis.svg`,
-	      enabled: true,
-	      selected: true,
-	      _data_tree_branch: [{
-	        text: 'List Item 1'
-	      },{
-	        text: 'List Item 2',
-	        checked: true
-	      },{
-	        text: 'List Item 3',
-	        text1: 'More text'
-	      },{
-	        text: 'List Item 4',
-	        status: 'status'
-	      },{
-	        text: 'List Item 5',
-	        icons: ['bell']
-	      }]
+	    	text: 'Tab 1',
+	    	icon: `${window.MODULE_PATH}/icons/analysis.svg`,
+	    	enabled: true,
+	    	selected: true,
+	    	_data_tree_branch: [
+	    		{
+	    		  	text: 'List Item 1'
+	    		},
+	    		{
+	    	  		text: 'List Item 2',
+	    	  		checked: true
+	    		},
+	    		{
+	    	  		text: 'List Item 3',
+	    	  		text1: 'More text'
+	    		},
+	    		{
+	    	  		text: 'List Item 4',
+	    	  		status: 'status'
+	    		},
+	    		{
+	    	  		text: 'List Item 5',
+	    	  		icons: ['bell']
+	    		}
+	    	]
 	    },
 	    {
-	      text: 'Tab 2',
-	      icon: `${window.MODULE_PATH}/icons/blood-sample.svg`,
-	      enabled: true,
-	      selected: false,
-	      _data_tree_branch: [{
-	        text: 'List Item 1',
-	        text1: 'Tab 2 List Item'
-	      },{
-	        text: 'List Item 2',
-	        text1: 'Tab 2 List Item'
-	      }]
+	    	text: 'Tab 2',
+	    	icon: `${window.MODULE_PATH}/icons/blood-sample.svg`,
+	    	enabled: false,
+	    	selected: false,
+	    	_data_tree_branch: [
+	    		{
+	    	  		text: 'List Item 1',
+	    	  		text1: 'Tab 2 List Item'
+	    		},
+	    		{
+	    	  		text: 'List Item 2',
+	    	  		text1: 'Tab 2 List Item'
+	    		}
+	    	]
 	    },
 	    {
 	    	text: 'Fetch',
+	    	icon: 'jpy',
 	    	fetchInfo: {
 	    		url: 'localhost:3000'
 	    	}
